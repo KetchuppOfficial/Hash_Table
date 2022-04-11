61b49764f8dc990ec71692d786d9ebcd28150223
KetchuppOfficial <dolgov04@list.ru>
2022-04-11 21:32:10 +0300

    Experimenting with hash functions

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include "Hash_Table.h"
 
+#define DUMP 0
+
 int main (void)
 {
     #if DEBUG == 1
     OPEN_LOG_FILE;
     #endif
     
-    struct Hash_Table *ht_ptr = HT_Ctor (SHA_256, 20);
+    struct Hash_Table *ht_ptr = HT_Ctor (SHA_256, 400);
 
-    HT_Fill (ht_ptr, "Hash_Research/Test.txt");
+    HT_Fill (ht_ptr, "Hash_Research/Hamlet.txt");
 
+    #if DUMP == 1
     HT_Dump (ht_ptr);
+    #endif
 
     HT_Print_Collisons (ht_ptr);
     
