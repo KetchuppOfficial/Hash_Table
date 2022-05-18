6ba7c3b5bb3d42643702f017f4586899df46322f
KetchuppOfficial <dolgov04@list.ru>
2022-05-18 11:51:02 +0300

    Axis ticks are now calculated automatically

--- Not_Optimized/src/Histogram.py
+++ Not_Optimized/src/Histogram.py
@@ -3,53 +3,79 @@ import numpy as np
 import sys
 import math
 
-# ========================================
-def Get_Round_Step (max_val, mult_base, n_ticks):
+# =========================================
+def N_Digits (n):
 
-    step = max_val / n_ticks
-    step = mult_base * (step // mult_base)
+    i = 0
+    while n > 0:
+        n = n // 10
+        i += 1
 
-    if step == 0:
-        return 1
-    
-    return step
-# ========================================
+    return i
+# =========================================
 
-graph_path = str(sys.argv[1])
-graph_name = str(sys.argv[2])
+# =========================================
+def Get_Round_Step (val):
 
-data_file = open(graph_path + graph_name + ".txt", "r")
-data = data_file.readlines()
-data_file.close()
+    n_digits = N_Digits (val)
+    pow_10 = 10 ** (n_digits - 1)
 
-n_values = len(data)
+    first_digit = val // pow_10
+    ceil = (first_digit + 1) * pow_10
 
-x = []
-for i in range(n_values):
-    x.append(i)
-    data[i] = int(data[i])
+    if val > 5 * pow_10:
+        floor = pow_10
+    else:
+        if first_digit == 1:
+            floor = pow_10 // 4
+        else:
+            floor = pow_10 // 2
 
-plt.figure (figsize = (16, 10), dpi = 80)
+    return floor
+# =========================================
 
-plt.bar(x, data, width = 1)
 
-plt.title(graph_name, fontsize = 40, loc = 'center')
+# ===============================================================================
+def main (argv):
 
-plt.xlabel("Bucket number", fontsize = 20)
-plt.xlim(-10, n_values + 10)
+    graph_path = str(argv[1])
+    graph_name = str(argv[2])
 
-step_x = Get_Round_Step (n_values, 10, 10)
-plt.xticks(np.arange(0, n_values + 1, step = step_x), fontsize = 16)
+    data_file = open(graph_path + graph_name + ".txt", "r")
+    data = data_file.readlines()
+    data_file.close()
 
-max_y = max(data)
-plt.ylabel("Number of elements", fontsize = 20)
-plt.ylim(0, 1.1 * max_y)
+    n_values = len(data)
 
-step_y = Get_Round_Step (max_y, 10, 10)
-plt.yticks(np.arange(0, max_y, step = step_y), fontsize = 16)
+    x = []
+    for i in range(n_values):
+        x.append(i)
+        data[i] = int(data[i])
 
-plt.text(n_values - 500, max_y, "Max collisions: %d" %max_y, fontsize = 20)
+    plt.figure (figsize = (16, 10), dpi = 80)
 
-plt.savefig(graph_path + graph_name + ".png")
+    plt.bar(x, data, width = 1)
 
-plt.clf()
+    plt.title(graph_name, fontsize = 40, loc = 'center')
+
+    plt.xlabel("Bucket number", fontsize = 20)
+    plt.xlim(-10, n_values + 10)
+
+    step_x = Get_Round_Step (n_values)
+    plt.xticks(np.arange(0, n_values + 1, step = step_x), fontsize = 16)
+
+    max_y = max(data)
+    plt.ylabel("Number of elements", fontsize = 20)
+    plt.ylim(0, 1.1 * max_y)
+
+    step_y = Get_Round_Step (max_y)
+    plt.yticks(np.arange(0, max_y, step = step_y), fontsize = 16)
+
+    plt.text(n_values - 500, max_y, "Max collisions: %d" %max_y, fontsize = 20)
+
+    plt.savefig(graph_path + graph_name + ".png")
+
+    plt.clf()
+# ===============================================================================
+
+main (sys.argv[0:])
